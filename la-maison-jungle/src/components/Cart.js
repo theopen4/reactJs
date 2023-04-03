b8c4571208f64679ee248a339c84b9b0e800e6ba
theopen4 <kielze86@gmail.com>
2023-04-03 21:28:25 +0100

    init styles components

---
+++ la-maison-jungle/src/components/Cart.js
@@ -0,0 +1,18 @@
+function Cart(){
+    const prixMonstera = 8;
+    const prixLiere = 10;
+    const prixBouquet = 15;
+    const totalPanier = prixMonstera + prixLiere + prixBouquet; 
+    return (
+        <div>
+            <h3>Panier</h3>
+            <ul>
+                <li>un monstera {prixMonstera}</li>
+                <li>un liere {prixLiere}</li>
+                <li>un bouquet de fleur{prixBouquet}</li>
+            </ul>
+            <p>le total du panier est de {totalPanier}</p>
+       </div>
+    )
+}
+export default Cart;
