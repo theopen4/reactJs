29fbc7f25129f37b12c0f8a091991906233fd564
theopen4 <kielze86@gmail.com>
2023-04-28 09:05:02 +0100

    init component

---
+++ react-pokemon-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React ,{FunctionComponent} from "react";
+
+const App: FunctionComponent = ()=>{
+    const name: string = 'hello react';
+
+    return(
+        <h1>HELLO {name}</h1>
+    )
+
+}
+
+export default App;
