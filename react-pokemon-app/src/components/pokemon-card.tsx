7ded178a32e05713f1b9a34b1e7855ffd29dba5a
theopen4 <kielze86@gmail.com>
2023-05-10 23:31:15 +0100

    create the component props

--- react-pokemon-app/src/components/pokemon-card.tsx
+++ react-pokemon-app/src/components/pokemon-card.tsx
@@ -1,6 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
 import './pokemon-card.css';
+import formatDate from '../helpers/format-date';
+import formatType from '../helpers/format-type';
+
   
 type Props = {
   pokemon: Pokemon
@@ -8,16 +11,30 @@ type Props = {
 };
   
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor='#009688'}) => {
+    const [color, setColor] = useState<string>();
+    const showBorder = () =>{
+        setColor(borderColor);
+    }
+    const hideBorder = () =>{
+        setColor('#f5f5f5');
+    }
+    
+    
   return (
-    <div className="col s6 m4">
-      <div className="card horizontal" style={{borderColor: borderColor}}>
+    <div className="col s6 m4" onMouseEnter = {showBorder} onMouseLeave={hideBorder}>
+      <div className="card horizontal" style={{borderColor: color}}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
         <div className="card-stacked">
           <div className="card-content">
             <p>{pokemon.name}</p>
-            <p><small>{pokemon.created.toString()}</small></p>
+            <p><small>{formatDate(pokemon.created)}</small></p>
+            {
+                pokemon.types.map(type =>(
+                    <span key={type} className={formatType(type)}>{type}</span>
+                ))
+            }
           </div>
         </div>
       </div> 
