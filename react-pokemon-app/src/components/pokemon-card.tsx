e64b78f8c98ac605f4dd0d72c2397742fd527a50
theopen4 <kielze86@gmail.com>
2023-05-10 19:35:27 +0100

    create the component props

--- react-pokemon-app/src/components/pokemon-card.tsx
+++ react-pokemon-app/src/components/pokemon-card.tsx
@@ -1,17 +1,28 @@
-import React, {FunctionComponent} from "react";
-import Pokemon from "../models/pokemon";
-
-type Props ={
-    pokemon: Pokemon
-
+import React, { FunctionComponent } from 'react';
+import Pokemon from '../models/pokemon';
+import './pokemon-card.css';
+  
+type Props = {
+  pokemon: Pokemon
+  borderColor?: string
 };
-const PokemonCard: FunctionComponent<Props> = ({pokemon}) =>{
-    return (
-        <div>
-            ce composant est charge d afficher les pokemons: {pokemon.name}
+  
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor='#009688'}) => {
+  return (
+    <div className="col s6 m4">
+      <div className="card horizontal" style={{borderColor: borderColor}}>
+        <div className="card-image"> 
+          <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
-    )
-
-} 
-
+        <div className="card-stacked">
+          <div className="card-content">
+            <p>{pokemon.name}</p>
+            <p><small>{pokemon.created.toString()}</small></p>
+          </div>
+        </div>
+      </div> 
+    </div>
+  );
+}
+  
 export default PokemonCard;
